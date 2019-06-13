957b261eeb9743c51f644b80c4d12b32a44944a9
Érick Mazzocco <erickmazzocco@gmail.com>
2019-06-13 19:55:07 -0300

    commit inicial

---
+++ Vidly2/Controllers/CustumersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustumersController : Controller
+    {
+        public ViewResult Index()
+        {
+            var custumers = GetCustumers();
+
+            return View(custumers);
+        }
+
+        public ViewResult Details(int Id)
+        {
+            return View(GetCustumers().First(x => x.Id == Id));
+        }
+
+        private IEnumerable<Custumer> GetCustumers()
+        {
+            return new List<Custumer>()
+            {
+                new Custumer()
+                {
+                    Id = 1, Name = "Linguiça"
+                },
+                new Custumer()
+                {
+                    Id = 2, Name = "Paçoca"
+                }
+            };
+        }
+
+        private IEnumerable<Custumer> GetEmptyCustumer()
+        {
+            return new List<Custumer>();
+        }
+        
+    }
+}
---
+++ Vidly2/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        public ViewResult Details(int Id)
+        {
+            return View(GetMovies().First(x => x.Id == Id));
+        }
+
+        private  IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie()
+                {
+                    Id = 1, Name = "Érick"
+                },
+                new Movie()
+                {
+                    Id = 2, Name = "Xuxu"
+                }
+            };
+        }
+
+    }
+}
